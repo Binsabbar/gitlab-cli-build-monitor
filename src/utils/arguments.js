5a712ed2cdf83f80bce3099afc792ffbca980f10
Mo <Binsabbar@users.noreply.github.com>
2020-04-18 17:09:29 +0300

    parse config yaml file

--- src/utils/arguments.js
+++ src/utils/arguments.js
@@ -1,14 +1,21 @@
+const fs = require('fs');
 const yargs = require('yargs');
+const yaml = require('js-yaml');
 
 const commandParser = yargs
   .options({
     f: {
       alias: 'file',
       demandOption: true,
-      describe: 'pipeline configuration file',
+      describe: 'path pipeline configuration file',
       string: true,
     },
   })
+  .config('file', 'path to pipeline configuration file',
+    ((configPath) => {
+      const config = yaml.safeLoad(fs.readFileSync(configPath, 'utf8'));
+      return config;
+    }))
   .help();
 
 exports.commandParser = commandParser;
