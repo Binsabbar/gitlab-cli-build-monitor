c8661e312ebbde12dce8a8d44367214871f7d565
binsabbar <ibinsabbar-os@gmail.com>
2020-04-18 20:53:57 +0300

    make option arg required + add more validation

--- __tests__/utils/arguments.test.js
+++ __tests__/utils/arguments.test.js
@@ -2,8 +2,11 @@
 const argument = require('../../src/utils/arguments');
 
 
-const parse = (commandParser, arg) => new Promise((resolve) => {
-  commandParser.parse(arg, (_, argvs, output) => resolve({ output, argvs }));
+const parse = (commandParser, arg) => new Promise((resolve, reject) => {
+  commandParser.parse(arg, (error, argvs, output) => {
+    if (error) reject(error);
+    resolve({ output, argvs });
+  });
 });
 
 describe('script argument', () => {
@@ -11,33 +14,41 @@ describe('script argument', () => {
   --version   Show version number                                      [boolean]
   -f, --file  path to pipeline configuration file            [string] [required]
   --help      Show help                                                [boolean]`;
+
   it('prints help menu when --help is given', async () => {
     const { output } = await parse(argument.commandParser, '--help');
 
     expect(output).toBe(expectedDefaultOutput);
   });
 
-  it('highlights missing argument when -f or --file are not given', async () => {
-    const { output } = await parse(argument.commandParser, '');
-
-    expect(output).toContain('Missing required argument: f');
+  it('demands -f or --file when they are not given', async () => {
+    await expect(parse(argument.commandParser, '')).rejects.toThrow('Missing required argument: f');
   });
 
-  describe('when demand options are given', () => {
-    it('does not show menu given -f', async () => {
-      const { output } = await parse(argument.commandParser, '-f');
+  describe('when arg for a given option is missing', () => {
+    const sharedTest = (opt) => {
+      it(`shows error when ${opt} args are missing`, async () => {
+        const errMsg = opt.replace(/-/g, '');
+        await expect(parse(argument.commandParser, `${opt}`))
+          .rejects.toThrow(`Not enough arguments following: ${errMsg}`);
+      });
+    };
 
-      expect(output).toEqual('');
-    });
-
-    it('does not show menu given --file', async () => {
-      const { output } = await parse(argument.commandParser, '--file');
-
-      expect(output).toEqual('');
-    });
+    ['-f', '--file'].forEach(sharedTest);
   });
 
-  describe('when config file is passed', () => {
+  describe('when arugments are passed to options', () => {
+    const sharedTest = (opt) => {
+      it(`does not show error for ${opt}`, async () => {
+        const { output } = await parse(argument.commandParser, `${opt} ./example-config.yml`);
+        expect(output).toEqual('');
+      });
+    };
+
+    ['-f', '--file'].forEach(sharedTest);
+  });
+
+  describe('when config file is given', () => {
     it('parses the file into argvs', async () => {
       const { argvs } = await parse(argument.commandParser, '--file ./example-config.yml');
       const expectedConfigs = {
@@ -49,6 +60,23 @@ describe('script argument', () => {
       };
 
       expect(argvs).toMatchObject(expectedConfigs);
+    });
+
+    it('returns error when configuration file is invalid', async () => {
+      const configFile = `${__dirname}/data/invalid-intervals-3.yml`;
+      await expect(parse(argument.commandParser, `--file ${configFile}`)).rejects.toThrow();
+    });
+
+    it('returns error when configuration file does not exist', async () => {
+      const configFile = `${__dirname}/data/i-do-no-exist.yml`;
+      await expect(parse(argument.commandParser, `--file ${configFile}`))
+        .rejects.toThrow('File does not exist');
+    });
+
+    it('returns error when configuration file is not a valid yaml', async () => {
+      const configFile = `${__dirname}/data/invalid-yaml.yml`;
+      await expect(parse(argument.commandParser, `--file ${configFile}`))
+        .rejects.toThrow('Invalid Yaml');
     });
   });
 });
--- src/utils/arguments.js
+++ src/utils/arguments.js
@@ -2,20 +2,32 @@ const fs = require('fs');
 const yargs = require('yargs');
 const yaml = require('js-yaml');
 
+const { validate } = require('./configValidator');
+
+const configParser = (configPath) => {
+  if (!fs.existsSync(configPath)) return new Error('File does not exist');
+  let config;
+  try {
+    config = yaml.safeLoad(fs.readFileSync(configPath, 'utf8'));
+  } catch (e) {
+    return new Error('Invalid Yaml');
+  }
+  const validation = validate(config);
+  if (validation.error) return new Error(validation.error);
+  return validation;
+};
+
 const commandParser = yargs
   .options({
     f: {
       alias: 'file',
       demandOption: true,
+      requiresArg: true,
       describe: 'path pipeline configuration file',
       string: true,
     },
   })
-  .config('file', 'path to pipeline configuration file',
-    ((configPath) => {
-      const config = yaml.safeLoad(fs.readFileSync(configPath, 'utf8'));
-      return config;
-    }))
+  .config('file', 'path to pipeline configuration file', configParser)
   .help();
 
 exports.commandParser = commandParser;
