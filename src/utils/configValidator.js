75ae5778e879d370643be7aaac9acfc22cfd43eb
binsabbar <ibinsabbar-os@gmail.com>
2020-04-18 19:50:44 +0300

    validate keys values

--- src/utils/configValidator.js
+++ src/utils/configValidator.js
@@ -1,14 +1,49 @@
 const requiredKeys = ['projects', 'gitlab_access_token', 'update_intervals'];
+const keysTypes = {
+  gitlab_access_token: {
+    validate: (arg) => typeof arg === 'string',
+    errMessage: 'gitlab_access_token must be string',
+  },
+  projects: {
+    validate: (arg) => Array.isArray(arg),
+    errMessage: 'projects must be array',
+  },
+  update_intervals: {
+    validate: (args) => typeof args === 'number',
+    errMessage: 'update_intervals must be number',
+  },
+};
+
+const errorMessage = (message) => ({ error: message });
+
 const containsElement = (arr, element) => arr.indexOf(element) > -1;
+
+const checkMissingKeys = (keys) => {
+  for (let i = 0; i < requiredKeys.length; i++) {
+    const requiredKey = requiredKeys[i];
+    if (!containsElement(keys, requiredKey)) return errorMessage(`${requiredKey} is missing`);
+  }
+};
+
+const validateKeysTypes = (config) => {
+  for (let i = 0; i < requiredKeys.length; i++) {
+    const key = requiredKeys[i];
+    const value = config[key];
+    const validateFunc = keysTypes[key].validate;
+    if (!validateFunc(value)) return errorMessage(keysTypes[key].errMessage);
+  }
+};
 
 const validate = (config) => {
   const keys = Object.keys(config);
-  for (let i = 0; i < requiredKeys.length; i++) {
-    const key = requiredKeys[i];
-    if (!containsElement(keys, key)) {
-      return { error: `${key} is missing` };
-    }
-  }
+
+  const missingKeysCheck = checkMissingKeys(keys);
+  if (missingKeysCheck) return missingKeysCheck;
+
+  const validateRes = validateKeysTypes(config);
+  if (validateRes) return validateRes;
+
+  return config;
 };
 
 exports.validate = validate;
