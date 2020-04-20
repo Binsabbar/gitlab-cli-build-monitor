502d354ded8f14a8730ed7cd7b15f71395c58a8a
Mo <Binsabbar@users.noreply.github.com>
2020-04-21 02:57:21 +0300

    add api and version path

--- __tests__/client/gitlabApiPaths.test.js
+++ __tests__/client/gitlabApiPaths.test.js
@@ -1,30 +1,31 @@
 const { gitlabApiPaths } = require('../../src/client/gitlabApiPaths');
 
 describe('gitlabApiPaths', () => {
+  const apiPath = '/api/v4';
   it('returns the api path for project given id', () => {
     const projectPath = gitlabApiPaths.project({ projectId: 24 });
-    const expected = '/projects/24/';
+    const expected = `${apiPath}/projects/24/`;
 
     expect(projectPath).toEqual(expected);
   });
 
   it('returns the api path for project pipelines given id', () => {
     const projectPath = gitlabApiPaths.pipelines({ projectId: 24 });
-    const expected = '/projects/24/pipelines/';
+    const expected = `${apiPath}/projects/24/pipelines/`;
 
     expect(projectPath).toEqual(expected);
   });
 
   it('returns the api path for a single pipeline given id', () => {
     const projectPath = gitlabApiPaths.projectPipeline({ projectId: 24, pipelineId: 44 });
-    const expected = '/projects/24/pipelines/44/';
+    const expected = `${apiPath}/projects/24/pipelines/44/`;
 
     expect(projectPath).toEqual(expected);
   });
 
   it('returns the api path for a single pipeline given id', () => {
     const projectPath = gitlabApiPaths.projectPipelineJobs({ projectId: 24, pipelineId: 44 });
-    const expected = '/projects/24/pipelines/44/jobs';
+    const expected = `${apiPath}/projects/24/pipelines/44/jobs`;
 
     expect(projectPath).toEqual(expected);
   });
