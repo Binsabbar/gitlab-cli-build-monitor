36dda290ca0a2f1f2ff241e9758deaae016af8be
Mo <Binsabbar@users.noreply.github.com>
2020-04-26 20:29:38 +0300

    make loopTask promisezed (is that a word?)

--- __tests__/services/scheduler.test.js
+++ __tests__/services/scheduler.test.js
@@ -6,23 +6,24 @@ describe('scheduler', () => {
   const twoSeconds = 2;
   const advanceTimeByMs = twoSeconds * 1000 * 5;
 
-  it('executes a callback every x seconds', () => {
-    const spyFunc = jest.fn();
-
-    loopTask(spyFunc, twoSeconds);
+  it('executes a callback every x seconds', (done) => {
+    const spyFunc = jest.fn(() => Promise.resolve(1));
+    loopTask(spyFunc, {}, twoSeconds).then((result) => { expect(result).toBe(1); done(); });
 
     expect(spyFunc).not.toBeCalled();
+
     jest.advanceTimersByTime(advanceTimeByMs);
     expect(spyFunc).toHaveBeenCalledTimes(5);
   });
 
-  it('stops a scheduler', () => {
-    const spyFunc = jest.fn();
+  it('stops a scheduler', (done) => {
+    const spyFunc = jest.fn(() => Promise.resolve(1));
+    loopTask(spyFunc, {}, twoSeconds).then((_) => done());
 
-    loopTask(spyFunc, twoSeconds);
     jest.advanceTimersByTime(advanceTimeByMs);
-
+    expect(spyFunc).toHaveBeenCalledTimes(5);
     stopTask();
+
     jest.advanceTimersByTime(advanceTimeByMs);
     expect(spyFunc).toHaveBeenCalledTimes(5);
   });
