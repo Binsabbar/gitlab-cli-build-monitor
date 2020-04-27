df5dc2165a607306c4b6c3da6e48740c3a6e27cc
Mo <Binsabbar@users.noreply.github.com>
2020-04-27 22:34:57 +0300

    handle none client errors in the message output

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,10 +1,27 @@
-const handleGitlabClientErrors = (gitlabClientError) => {
-  let message = 'The following errors occured\n';
-  message += 'projectId: status\n';
-  gitlabClientError.forEach((error) => {
-    message += `${error.projectId}: ${error.status}\n`;
+const Table = require('cli-table3');
+
+const formatFinalMessage = (clientErrors, otherErros) => {
+  if (clientErrors.length > 0 && otherErros.length === 0) {
+    return clientErrors.toString();
+  } if (otherErros.length > 0 && clientErrors.length === 0) {
+    return otherErros.toString();
+  }
+  return `${clientErrors.toString()}\n${otherErros.toString()}`;
+};
+
+const handleGitlabClientErrors = (errors) => {
+  const clientErrors = new Table({ head: ['Project ID', 'Status'] });
+  const otherErros = new Table({ head: ['Message'] });
+
+  errors.forEach((error) => {
+    if (error.status && error.projectId) {
+      clientErrors.push([error.projectId, error.status]);
+    } else {
+      otherErros.push([error.message]);
+    }
   });
-  return message;
+
+  return formatFinalMessage(clientErrors, otherErros);
 };
 
 exports.handleGitlabClientErrors = handleGitlabClientErrors;
