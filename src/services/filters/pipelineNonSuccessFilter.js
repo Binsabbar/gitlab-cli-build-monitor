a96a918e655e8364899412fc57c13ab17778c702
binsabbar <ibinsabbar@gmail.com>
2020-04-28 04:35:51 +0300

    always including a running pipeline

--- src/services/filters/pipelineNonSuccessFilter.js
+++ src/services/filters/pipelineNonSuccessFilter.js
@@ -7,29 +7,30 @@ const NON_SUCCESS_STATUS = {
 };
 const NONE_SUCCESS_VALUES = Object.values(NON_SUCCESS_STATUS);
 
+const isRunning = (pipeline) => pipeline.status === NON_SUCCESS_STATUS.RUNNING;
 const isNoneSuccess = (pipeline) => {
   const { status } = pipeline;
   return NONE_SUCCESS_VALUES.indexOf(status) !== -1;
 };
 
 const getLastUpdated = (pipelineA, pipelineB) => {
-  if (moment(pipelineA.updatedAt).isAfter(pipelineB.updatedAt)) { return pipelineA; }
+  if (moment(pipelineA.updatedAt).isAfter(pipelineB.updatedAt)) return pipelineA;
   return pipelineB;
 };
 
 const filter = (pipelines) => {
   const pipelinesByRefs = {};
+  const runningPipelines = [];
+
   pipelines.forEach((pipeline) => {
-    if (isNoneSuccess(pipeline)) {
-      if (!pipelinesByRefs[pipeline.ref]) {
-        pipelinesByRefs[pipeline.ref] = pipeline;
-      } else {
-        pipelinesByRefs[pipeline.ref] = getLastUpdated(pipelinesByRefs[pipeline.ref], pipeline);
-      }
+    if (isRunning(pipeline)) runningPipelines.push(pipeline);
+    else if (isNoneSuccess(pipeline)) {
+      const { ref } = pipeline;
+      pipelinesByRefs[pipeline.ref] = getLastUpdated(pipelinesByRefs[ref] || pipeline, pipeline);
     }
   });
 
-  return Object.values(pipelinesByRefs).flat();
+  return Object.values(pipelinesByRefs).flat().concat(runningPipelines);
 };
 
 exports.filter = filter;
