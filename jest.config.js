4568725b1e358aa076af39b1b0ba0982af77ff6b
binsabbar <ibinsabbar@gmail.com>
2020-04-28 23:51:55 +0300

    update coverage paths

--- jest.config.js
+++ jest.config.js
@@ -19,17 +19,19 @@ module.exports = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    'src/**/*.js',
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: [
-    '/node_modules/',
+    '/node_modules/', 'src/index.local.js',
   ],
 
   // A list of reporter names that Jest uses when writing coverage reports
