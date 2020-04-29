2956340e4808b4004a2d1125c2bde22e50cb26ae
binsabbar <ibinsabbar@gmail.com>
2020-04-30 00:52:19 +0300

    get jobs for a project

--- __tests__/services/monitors/projectMonitorService.test.js
+++ __tests__/services/monitors/projectMonitorService.test.js
@@ -17,9 +17,21 @@ describe('ProjectMonitorService', () => {
     new PipelineBuilder().withId('12').build(),
     new PipelineBuilder().withId('13').build(),
   ];
-  const Jobs = [new JobBuilder().withId(12).build(), new JobBuilder().withId(14).build()];
+  const jobsForPipeline12 = [
+    new JobBuilder().withId(12).build(),
+    new JobBuilder().withId(14).build(),
+  ];
+  const jobsForPipeline13 = [
+    new JobBuilder().withId(15).build(),
+    new JobBuilder().withId(16).build(),
+  ];
+
   const gitlabClient = new GitlabClient();
-  const monitor = new ProjectMonitorService({ gitlabClient });
+  const pipelineMonitorService = new PipelineMonitorService();
+  const jobMonitorService = new JobMonitorService();
+  const monitor = new ProjectMonitorService(
+    { gitlabClient, pipelineMonitorService, jobMonitorService },
+  );
 
   beforeEach(() => {
     jest.resetAllMocks();
@@ -46,6 +58,58 @@ describe('ProjectMonitorService', () => {
         .mockRejectedValue(new GitlabClientError({ status: 505, project: project.id }));
 
       return expect(monitor.doesProjectExist({ project })).rejects.toThrow();
+    });
+  });
+
+  describe('getJobs', () => {
+    it('returns a resolved promise with jobs from all pipelines', () => {
+      when(pipelineMonitorService.getPipelines)
+        .calledWith({ project }).mockResolvedValue(pipelines);
+      when(jobMonitorService.getJobs)
+        .calledWith({ project, pipeline: pipelines[0] }).mockResolvedValue(jobsForPipeline12);
+      when(jobMonitorService.getJobs)
+        .calledWith({ project, pipeline: pipelines[1] }).mockResolvedValue(jobsForPipeline13);
+
+      return expect(monitor.getJobs({ project })).resolves
+        .toIncludeSameMembers(jobsForPipeline12.concat(jobsForPipeline13));
+    });
+
+    it('returns a resolved promise with empty array if no pipeline exists', () => {
+      when(pipelineMonitorService.getPipelines)
+        .calledWith({ project }).mockResolvedValue([]);
+
+      return expect(monitor.getJobs({ project })).resolves.toEqual([]);
+    });
+
+    it('returns a resolved promise with empty array if no jobs exists', () => {
+      when(pipelineMonitorService.getPipelines)
+        .calledWith({ project }).mockResolvedValue(pipelines);
+      when(jobMonitorService.getJobs)
+        .calledWith({ project, pipeline: pipelines[0] }).mockResolvedValue([]);
+      when(jobMonitorService.getJobs)
+        .calledWith({ project, pipeline: pipelines[1] }).mockResolvedValue([]);
+
+      return expect(monitor.getJobs({ project })).resolves.toEqual([]);
+    });
+
+    it('returns a rejected promise if error occurs when getting pipelines', () => {
+      when(pipelineMonitorService.getPipelines)
+        .calledWith({ project }).mockRejectedValue(new Error('error happens'));
+
+
+      return expect(monitor.getJobs({ project })).rejects.toThrow();
+    });
+
+    it('returns a rejected promise if error occurs when getting jobs', () => {
+      when(pipelineMonitorService.getPipelines)
+        .calledWith({ project }).mockResolvedValue(pipelines);
+      when(jobMonitorService.getJobs)
+        .calledWith({ project, pipeline: pipelines[0] }).mockResolvedValue([]);
+      when(jobMonitorService.getJobs)
+        .calledWith({ project, pipeline: pipelines[1] }).mockRejectedValue(new Error('err'));
+
+
+      return expect(monitor.getJobs({ project })).rejects.toThrow();
     });
   });
 });
--- src/services/monitors/projectMonitorService.js
+++ src/services/monitors/projectMonitorService.js
@@ -1,17 +1,30 @@
 /* eslint-disable no-unused-vars */
 
 class ProjectMonitorService {
-  constructor({ gitlabClient }) {
-    this.gitlabClient = gitlabClient;
+  constructor({ gitlabClient, pipelineMonitorService, jobMonitorService }) {
+    this.client = gitlabClient;
+    this.pipelineService = pipelineMonitorService;
+    this.jobService = jobMonitorService;
   }
 
   doesProjectExist({ project }) {
     const { id: projectId } = project;
-    return this.gitlabClient.getProject({ projectId })
+    return this.client.getProject({ projectId })
       .then((_) => true)
       .catch((error) => {
-        if (error.status && error.status === 404) return false;
+        if (error.status === 404) return false;
         return Promise.reject(error);
+      });
+  }
+
+  getJobs({ project }) {
+    return this.pipelineService.getPipelines({ project })
+      .then((pipelines) => {
+        const promises = [];
+        pipelines.forEach((pipeline) => {
+          promises.push(this.jobService.getJobs({ project, pipeline }));
+        });
+        return Promise.all(promises).then((values) => values.flat());
       });
   }
 }
