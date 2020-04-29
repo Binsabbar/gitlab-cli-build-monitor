a56c04fffbff087ff92bbe4f48eb00b48b2b2e37
Mo <Binsabbar@users.noreply.github.com>
2020-04-29 23:27:57 +0300

    improve test

--- __tests__/services/filters/filterFactory.test.js
+++ __tests__/services/filters/filterFactory.test.js
@@ -2,14 +2,16 @@ jest.mock('../../../src/services/filters');
 const { getFilter, FILTER_TYPE } = jest.requireActual('../../../src/services/filters');
 const { pipelineNonSuccessFilter, jobNonSuccessFilter } = require('../../../src/services/filters');
 
-
 describe('filterFactory', () => {
+  pipelineNonSuccessFilter.mockReturnValue(1);
+  jobNonSuccessFilter.mockReturnValue(2);
   it('returns pipeline filter function when given pipeline type', () => {
     const type = FILTER_TYPE.PIPELINE;
 
     const filter = getFilter({ type });
 
     expect(filter).toEqual(pipelineNonSuccessFilter);
+    expect(filter()).toEqual(1);
   });
 
   it('returns pipeline filter function when given job type', () => {
@@ -18,5 +20,6 @@ describe('filterFactory', () => {
     const filter = getFilter({ type });
 
     expect(filter).toEqual(jobNonSuccessFilter);
+    expect(filter()).toEqual(2);
   });
 });
