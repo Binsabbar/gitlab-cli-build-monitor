ede0a16c38cdfa21f4d8ae2ef6bdde520d85108c
Mo <Binsabbar@users.noreply.github.com>
2020-04-30 01:58:54 +0300

    return list of jobs for each project

--- __tests__/services/monitorService.test.js
+++ __tests__/services/monitorService.test.js
@@ -10,7 +10,12 @@ const { ProjectMonitorService } = require('../../src/services/monitors');
 describe('MonitorService', () => {
   const projectIds = ['A', 'B', 'C'];
   const projectMonitorService = new ProjectMonitorService();
+
   const monitor = new MonitorService({ projectMonitorService });
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
 
   describe('doProjectExists', () => {
     it('returns a resolved promise with true if all projects exist', () => {
@@ -31,6 +36,41 @@ describe('MonitorService', () => {
         .mockRejectedValueOnce(new Error('error'));
 
       return expect(monitor.doProjectsExist({ projectIds })).rejects.toThrowError('error');
+    });
+  });
+
+  describe('checkStatus', () => {
+    const jobsForA = [new JobBuilder().withId(1).build(), new JobBuilder().withId(2).build()];
+    const jobsForB = [new JobBuilder().withId(21).build(), new JobBuilder().withId(22).build()];
+    const jobsForC = [new JobBuilder().withId(31).build(), new JobBuilder().withId(32).build()];
+
+    it('returns a resolved promise with list of jobs for each project', () => {
+      when(projectMonitorService.getJobs).calledWith({ projectId: projectIds[0] })
+        .mockResolvedValueOnce(jobsForA);
+      when(projectMonitorService.getJobs).calledWith({ projectId: projectIds[1] })
+        .mockResolvedValueOnce(jobsForB);
+      when(projectMonitorService.getJobs).calledWith({ projectId: projectIds[2] })
+        .mockResolvedValueOnce(jobsForC);
+
+      const expected = [
+        { projectId: 'A', jobs: jobsForA },
+        { projectId: 'B', jobs: jobsForB },
+        { projectId: 'C', jobs: jobsForC },
+      ];
+      return expect(monitor.checkStatus({ projectIds }))
+        .resolves.toIncludeSameMembers(expected);
+    });
+
+    it('returns a rejected promise when an error occurs for at least one project', () => {
+      when(projectMonitorService.getJobs).calledWith({ projectId: projectIds[0] })
+        .mockResolvedValueOnce(jobsForA);
+      when(projectMonitorService.getJobs).calledWith({ projectId: projectIds[1] })
+        .mockResolvedValueOnce(jobsForB);
+      when(projectMonitorService.getJobs).calledWith({ projectId: projectIds[2] })
+        .mockRejectedValueOnce(new Error('error'));
+
+      return expect(monitor.checkStatus({ projectIds }))
+        .rejects.toThrowError('error');
     });
   });
 });
