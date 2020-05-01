224d984263c1ebe26e87e809a87aebdeac12f781
Mo <Binsabbar@users.noreply.github.com>
2020-05-01 04:55:14 +0300

    implement cli entry + ignore its coverage
    - it is not easy to test the cli that takes args

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
+const { handleGitlabClientErrors, commandParser, GitlabConfig } = require('./utils');
+const { getService, SERVICES } = require('./objectsInitializer');
+const { Screen, ConsoleTableView } = require('./view');
 
-const { commandParser } = require('./utils/arguments');
+const gitlabConfig = new GitlabConfig({ ...commandParser.argv });
+const monitor = getService(SERVICES.MONITOR_SERVICE, gitlabConfig);
+const consoleTableView = new ConsoleTableView();
+const screen = new Screen();
 
-const { accessToken, projects, updateIntervals } = commandParser.argv;
-console.log({ accessToken, projects, updateIntervals });
+function printStatus() {
+  const log = consoleTableView.toPrintableTable();
+  consoleTableView.clearRows();
+  screen.screenWrite(log);
+}
+
+setInterval(() => {
+  monitor.checkStatus({ projectIds: gitlabConfig.projects })
+    .then((status) => {
+      status.forEach((s) => {
+        s.jobs.forEach((job) => consoleTableView.addStatusRow({ projectId: s.projectId, job }));
+      });
+      printStatus();
+    })
+    .catch((e) => {
+      console.log(handleGitlabClientErrors(e));
+    });
+}, gitlabConfig.updateIntervals * 1000);
